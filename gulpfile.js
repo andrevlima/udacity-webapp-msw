7a8426f2cddb313ccaef17c98212e4e1cb669a9f
NB24054 <andre.vinicius.lima@novabase.pt>
2018-05-21 00:33:49 +0100

    Gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,18 @@ const mincss = require('gulp-clean-css');
 const minjs = require('gulp-minify');
 const imagemin = require('gulp-imagemin');
 
+/**
+ * CSS preprocessing
+ */
 gulp.task('minify-css', () => {
     return gulp.src('css/*.css')
         .pipe(mincss({compatibility: 'ie8'}))
         .pipe(gulp.dest('dist/css'));
 });
 
+/**
+ * JavaScript preprocessing
+ */
 gulp.task('minify-js', () => {
     return gulp.src('js/*.js')
     .pipe(minjs({
@@ -20,10 +26,16 @@ gulp.task('minify-js', () => {
     .pipe(gulp.dest('dist/js'));
 });
 
+/**
+ * Image preprocessing
+ */
 gulp.task('minify-img', () =>
     gulp.src('img/*.*')
         .pipe(imagemin())
         .pipe(gulp.dest('dist/img'))
 );
   
+/**
+ * Default task profile
+ */
 gulp.task('default', [ 'minify-css', 'minify-js', 'minify-img' ]);
